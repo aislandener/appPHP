b0e176f326cc4ec5fbe6db091a22e310e6dae0ac
Aislan Dener Souza Vicentini <aislandenersouzavicentini@gmail.com>
2015-09-18 19:37:39 -0300

    colocando Bootstrap

--- inc/cabecalho.inc.php
+++ inc/cabecalho.inc.php
@@ -4,7 +4,13 @@
         <meta charset="UTF-8">
         <title></title>
     </head>
+    <link rel="stylesheet" type="text/css" href="/appPHP/assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/appPHP/assets/css/estilos.css">
     <script type="text/javascript" src ='/appPHP/assets/js/jquery-1.11.3.min.js'></script>
+    <script type="text/javascript" src ='/appPHP/assets/js/bootstrap.min.js'></script>
     <body>
     	<div class="corpo">
+
+    	<a href="/appPHP/cadastrar.php">Cadastrar</a>
+        <a href="/appPHP/listar.php">Listagem</a>
+        <a href="/appPHP/relatorio.php">Relatorio</a>
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php include './inc/cabecalho.inc.php' ?>
-        <a href="/appPHP/cadastrar.php">Cadastrar</a>
-        <a href="/appPHP/listar.php">Listagem</a>
-        <a href="/appPHP/relatorio.php">Relatorio</a>
+        
 
 <?php include './inc/rodape.inc.php' ?>
 
